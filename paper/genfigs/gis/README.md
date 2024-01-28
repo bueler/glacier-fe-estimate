f5766c52315372f51bec12aec8be13b43e312bd4
Ed Bueler <elbueler@alaska.edu>
2024-01-28 11:56:58 -0900

    add Greenland cross-section figure

---
+++ paper/genfigs/gis/README.md
@@ -0,0 +1,17 @@
+# glacier-fe-estimate/paper/genfigs/gis/
+
+In 2021 Andy Aschwanden sent me the data `gris_cross_profile.csv` from (https://nsidc.org/data/IDBMG4/versions/3)[BedMachine] processed by QGis, and a script to read it using pandas.  See `../giscross.py`.
+
+Cite BedMachine:
+```
+@Article{Morlighemetal2017,
+author = {Morlighem, M. and others},
+title = {{BedMachine v3: Complete bed topography and ocean bathymetry mapping of Greenland from multibeam echo sounding combined with mass conservation}},
+journal = {Geophysical Research Letters},
+volume = {44},
+number = {21},
+pages = {11,051-11,061},
+doi = {https://doi.org/10.1002/2017GL074954},
+year = {2017},
+}
+```
---
+++ paper/genfigs/giscross.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+df = pd.read_csv('gis/gris_cross_profile_lat_70.csv')
+x = np.array(df["Profile [m]"]) / 1000.0
+b = np.array(df["Bed [m]"])
+s0 = np.array(df["Surface [m]"])
+s = s0.copy()
+s[s0 < 10.0] = b[s0 < 10.0]
+H = s - b
+L = 750.0
+
+# s(x) and b(x) versus x, with 100x exaggeration
+plt.figure(figsize=(10,7))
+plt.subplot(2,1,1)
+plt.plot(x,s,'k',label='surface $s$')
+plt.plot(x,b,'k--',label='bed $b$')
+plt.grid(True)
+plt.xlabel('')
+plt.ylabel('elevation (m)',fontsize=14.0)
+plt.xticks(fontsize=14.0)
+plt.yticks(fontsize=14.0)
+plt.minorticks_off()
+plt.axis([0.0,L,-1000.0,3500.0])
+plt.legend(loc='upper left',fontsize=14.0)
+fig = plt.subplot(2,1,2)
+plt.plot(x,H,'k')
+plt.grid(True)
+plt.xlabel('$x$ (km)',fontsize=14.0)
+plt.ylabel('thickness $H$ (m)',fontsize=14.0)
+plt.gca().yaxis.set_label_coords(-0.1,0.5)  # fiddle with location
+plt.xticks(fontsize=14.0)
+plt.yticks(fontsize=14.0)
+plt.minorticks_off()
+plt.axis([0.0,L,-100.0,3500.0])
+im = plt.imread('gis/gris-profile-gray.png')  # image is 200 pixels tall
+plt.figimage(im, 135.0, 100.0)  # offset in pixels
+print('writing giscross.pdf ...')
+plt.savefig('giscross.pdf', bbox_inches='tight')
